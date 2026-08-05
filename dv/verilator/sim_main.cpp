f2083a3c46b4bb44dc179968531d39076f2a68d3
icy-tee <nisitt321@gmail.com>
2026-08-05 17:13:34 -0400

    added entry point for observing obi transactions in verilator sim

--- dv/verilator/sim_main.cpp
+++ dv/verilator/sim_main.cpp
@@ -1,14 +1,23 @@
 
 #include <Vtop_verilator.h>
+#include <Vtop_verilator__Dpi.h>
 
 #include <verilated.h>
 #include <cstdio>
 #include <deque>
 #include <cstring>
 
+#include <fcntl.h>
+
 #include "sim_drv8833.hpp"
 #include "sim_pp_system.hpp"
 #include "sim_pwm.hpp"
+
+struct Settings {
+  size_t max_cycles = 6'000'000;
+  bool piped = false; // output tx to '/tmp/uart_tx', input rx from '/tmp/uart_rx'
+  int rx_pipe = -1, tx_pipe = -1;
+};
 
 struct Status {
     std::deque<std::string> &buf;
@@ -18,33 +27,53 @@ struct Status {
     double pwm_duty = 0.0;
 };
 
-void print_status(const Status &sts) {
+void print_status(const Settings &stgs, const Status &sts) {
     printf("\033[2J");
     printf("\033[H");
     printf("pwm: p%zu, d%.0f%%\n", sts.pwm_period, sts.pwm_duty);
     printf("x: fwd%.2f bwd%.2f cst%.2f brk%.2f\n", sts.x.fwd_ratio, sts.x.bwd_ratio, sts.x.cst_ratio, sts.x.brk_ratio);
     printf("y: fwd%.2f bwd%.2f cst%.2f brk%.2f\n", sts.y.fwd_ratio, sts.y.bwd_ratio, sts.y.cst_ratio, sts.y.brk_ratio);
-    printf("uart:\n");
-    for (std::string &line : sts.buf) {
-        printf("%s\n", line.c_str());
+
+    if (!stgs.piped) {
+        printf("uart:\n");
+        for (std::string &line : sts.buf) {
+            printf("%s\n", line.c_str());
+        }
     }
 }
 
-int main(int argc, const char **argv) { // usage: [name] [--cycles N]
-    size_t max_cycles = 6000000;
+void host_gnt_received(const svBitVecVal *addr, const svBitVecVal *wdata, svBit we, const svBitVecVal *be) {
+}
 
+void host_rvalid_received(const svBitVecVal *rdata, svBit err) {
+}
+
+
+int main(int argc, const char **argv) { // usage: [name] [--cycles N]
+
+    Settings settings;
+    
     for (size_t i = 1; i < argc; i++) {
         if (strncmp(argv[i], "--cycles", sizeof("--cycles")) == 0) {
             if (i + 1 >= argc) {
                 printf("Expected argument\nusage: %s [--cycles N]\n", argv[0]);
                 return 1;
             } else {
-                max_cycles = atoi(argv[++i]);
+                settings.max_cycles = atoi(argv[++i]); // TODO: add error handling/sanitation
             }
+        } else if (strncmp(argv[i], "--piped", sizeof("--piped")) == 0) {
+            settings.piped = true;
         } else {
             printf("Unrecognized argument\nusage: %s [--cycles N]\n", argv[0]);
             return 1;
         }
+    }
+
+    if (settings.piped) {
+        settings.tx_pipe = open("/tmp/uart_tx", O_RDWR | O_NONBLOCK); // so they dont block
+        settings.rx_pipe = open("/tmp/uart_rx", O_RDWR | O_NONBLOCK);
+
+        printf("rx_pipe=%d, tx_pipe=%d\n", settings.rx_pipe, settings.tx_pipe);
     }
     
     VerilatedContext context;
@@ -83,25 +112,35 @@ int main(int argc, const char **argv) { // usage: [name] [--cycles N]
 
 
     system.set_uart_decode_cb([&](uint8_t output) {
-        if (output == '\n') {
-            uart_lines.push_back(uart_tmp);
-            uart_tmp.clear();
 
-            if (uart_lines.size() > 32) {
-                uart_lines.pop_front();
+        if (!settings.piped) {
+            if (output == '\n') {
+                uart_lines.push_back(uart_tmp);
+                uart_tmp.clear();
+
+                if (uart_lines.size() > 32) {
+                    uart_lines.pop_front();
+                }
+            } else if (output != '\r') {
+                uart_tmp.push_back(output);
             }
-        } else if (output != '\r') {
-            uart_tmp.push_back(output);
+        } else {
+            (void)write(settings.tx_pipe, &output, 1);
         }
     });
 
     system.reset();
 
-    for (size_t cycles = 0; cycles < max_cycles; cycles++) {
+    char buf = 0;
+    for (size_t cycles = 0; cycles < settings.max_cycles; cycles++) {
+        if (read(settings.rx_pipe, &buf, 1) > 0) {
+            system.uart_enqueue(buf);
+        }
+        
         system.tick();
 
         if (cycles % 10'000 == 0) {
-            print_status(sts);
+            print_status(settings, sts);
         }
     }
 
--- rtl/platform/top_verilator.sv
+++ rtl/platform/top_verilator.sv
@@ -15,7 +15,7 @@ module top_verilator #(
 
 pp_system #(
     .SRAMInitFile(SRAMInitFile)
-) pp_system (
+) u_pp_system (
     .clk,
     .rst_n,
     .quad_x,
@@ -26,5 +26,38 @@ pp_system #(
     .motor_y,
     .pwm(servo)
 );
+
+import "DPI-C" function void host_gnt_received (
+        input bit [31:0] addr,
+        input bit [31:0] wdata,
+        input bit we,
+        input bit [3:0] be
+);
+
+import "DPI-C" function void host_rvalid_received (
+        input bit [31:0] rdata,
+        input bit err
+);
+
+always_ff @(posedge clk or negedge rst_n) begin
+    if (rst_n) begin
+        if (u_pp_system.host_req[0] &&
+            u_pp_system.host_gnt[0]) begin
+            host_gnt_received(
+                u_pp_system.host_addr [0],
+                u_pp_system.host_wdata[0],
+                u_pp_system.host_we   [0],
+                u_pp_system.host_be   [0]
+            );
+        end
+
+        if (u_pp_system.host_rvalid[0]) begin
+            host_rvalid_received(
+                u_pp_system.host_rdata[0],
+                u_pp_system.host_err  [0]
+            );
+        end
+    end
+end
 
 endmodule
